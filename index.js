1a2e4640b0da269c3f2a015a5039262c690aa16c
Robert <robert.metz9@yahoo.com>
2021-02-02 12:07:21 -0800

    First Commit!!!

---
+++ index.js
@@ -0,0 +1,15 @@
+var itemPrice = 9.99;
+var moneyInWallet = 120;
+
+var friendCount = 100;
+
+var yearsOld = 25;
+
+var firstName = "Robert";
+var lastName = 'Metz';
+var middleInitial = 'B';
+
+
+console.log("I had " + moneyInWallet + "  dollars but after spending " + itemPrice + " on that candybar I only have " + (moneyInWallet - itemPrice) + " remaining");
+console.log("I am " + yearsOld + " and I have " + friendCount + " friends, that means I have made " + (friendCount/yearsOld) + " friends on average every year I have been alive!");
+console.log("My full name is " + firstName + " " + middleInitial + " " + lastName);
